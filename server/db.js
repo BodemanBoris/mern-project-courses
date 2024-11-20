3954c4a5a1bd8c4f25aea9e5b1b62793723b12a6
BodemanBoris <borisbodemand@hotmail.com>
2024-11-20 19:25:40 +0100

    Probando cargar desde la pc nueva

--- server/db.js
+++ server/db.js
@@ -1,11 +1,12 @@
 import mongoose from "mongoose";
 
-export const connectDb =async()=>{
-    try {
-        
-        await mongoose.connect("mongodb+srv://borisdortmundbvb:Bodeman.01@fabyrosealumnos.9lliq.mongodb.net/?retryWrites=true&w=majority&appName=FabyRoseAlumnos")
-        console.log("Db is connected")
-    } catch (error) {
-        console.log("Error DB ---- ", error)
-    }
-}
+export const connectDb = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://borisdortmundbvb:borisBodemanD@fabyrosealumnos.9lliq.mongodb.net/?retryWrites=true&w=majority&appName=FabyRoseAlumnos"
+    );
+    console.log("Db is connected");
+  } catch (error) {
+    console.log("Error DB ---- ", error);
+  }
+};
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,11 +1,9 @@
-import { Router } from 'express'
-import { logIn, logOut, signIn, user } from '../controller/authController.js'
+import { Router } from "express";
+import { logIn, logOut, signIn, user } from "../controller/authController.js";
 
-export const autRoutes = Router()
+export const autRoutes = Router();
 
-autRoutes.post('/login', logIn)
-autRoutes.post('/signin', signIn)
-
-autRoutes.post('/logout', logOut)
-
-autRoutes.get('/user', user)
+autRoutes.post("/login", logIn);
+autRoutes.post("/signin", signIn);
+autRoutes.post("/logout", logOut);
+autRoutes.get("/user", user);
